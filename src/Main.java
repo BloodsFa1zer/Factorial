6507c85b050c620187eb0640b85a33e39fe8f6e6
BloodsFa1zer <62332655+BloodsFa1zer@users.noreply.github.com>
2020-04-12 13:44:24 +0300

    ghnfr

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Recursion recursion = new Recursion();
+        System.out.println(recursion.calculateFactorial(n));
+
+    }
+}
---
+++ src/Recursion.java
@@ -0,0 +1,14 @@
+public class Recursion {
+    public static int calculateFactorial(int n) {
+
+        while (n == 1) {
+            if (n == 1) {
+                return 1;
+            }
+            return calculateFactorial(n - 1) * n;
+
+        }
+        return calculateFactorial(n - 1) * n;
+
+    }
+}
